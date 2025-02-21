39f3674d9761794fc4ed1e2bd68b1b1634fffbd9
Glebegor <glebegor2220@gmail.com>
2025-02-21 11:41:22 +0100

    ALL: Setuped processing architecture.

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import sys
+ 
+import pygame
+from pygame.locals import *
 
-def main():
-    pass
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.fps = 60.0
+        self.fpsClock = pygame.time.Clock()
+        self.width = 1024
+        self.height = 868
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.dt = 1/self.fps
+        
+
+    
+    def run():
+        # event handler
+        # post processing
+        # drawing picture
+        pass
 
 if __name__=="__main__":
-    main()
+    main = Main()
+    main.run()
